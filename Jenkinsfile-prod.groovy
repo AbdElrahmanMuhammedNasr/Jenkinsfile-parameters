209c4f0dcdbd0aa4e73586f894d1bc6b37214300
3bdo <34967675+AbdElrahmanMuhammedNasr@users.noreply.github.com>
2025-10-30 12:34:31 +0300

    Remove agent specification from Jenkinsfile

--- Jenkinsfile-prod.groovy
+++ Jenkinsfile-prod.groovy
@@ -1,7 +1,6 @@
-pipeline {
-    agent {label 'agent2'}
+ 
     stages {
         stage('Build') { steps { echo 'Building PROD...' } }
         stage('Deploy') { steps { echo 'Deploying to PROD...' } }
     }
-}
+

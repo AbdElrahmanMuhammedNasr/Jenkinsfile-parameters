48551a9b463301ac1d6c1a3ef4971a17172f1e77
3bdo <34967675+AbdElrahmanMuhammedNasr@users.noreply.github.com>
2025-10-30 12:34:09 +0300

    Remove agent specification from Jenkinsfile

--- Jenkinsfile-dev.groovy
+++ Jenkinsfile-dev.groovy
@@ -1,7 +1,5 @@
-pipeline {
-    agent {label 'agent2'}
+ 
     stages {
         stage('Build') { steps { echo 'Building DEV...' } }
         stage('Deploy') { steps { echo 'Deploying to DEV...' } }
     }
-}
